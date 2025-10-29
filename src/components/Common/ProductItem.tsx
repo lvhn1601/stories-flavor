ed1f7fbf826fa6c97715fcc5612969b1470dc642
sheep <lvhn1601@gmail.com>
2025-10-30 00:14:47 +0700

    [fix] product item

--- src/components/Common/ProductItem.tsx
+++ src/components/Common/ProductItem.tsx
@@ -28,24 +28,24 @@ const ProductItem = ({
   };
 
   // add to cart
-  const handleAddToCart = () => {
-    dispatch(
-      addItemToCart({
-        ...item,
-        quantity: 1,
-      })
-    );
-  };
+  // const handleAddToCart = () => {
+  //   dispatch(
+  //     addItemToCart({
+  //       ...item,
+  //       quantity: 1,
+  //     })
+  //   );
+  // };
 
-  const handleItemToWishList = () => {
-    dispatch(
-      addItemToWishlist({
-        ...item,
-        status: "available",
-        quantity: 1,
-      })
-    );
-  };
+  // const handleItemToWishList = () => {
+  //   dispatch(
+  //     addItemToWishlist({
+  //       ...item,
+  //       status: "available",
+  //       quantity: 1,
+  //     })
+  //   );
+  // };
 
   const handleProductDetails = () => {
     dispatch(updateproductDetails({ ...item }));
@@ -54,7 +54,7 @@ const ProductItem = ({
   return (
     <div className="group">
       <div className="relative overflow-hidden flex items-center justify-center rounded-lg bg-[#F6F7FB] min-h-[270px] mb-4">
-        <Image src={item.imgs.previews[0]} alt="" width={250} height={250} />
+        <Image src={item.images[0]} alt="" width={250} height={250} />
 
         <div className="absolute left-0 bottom-0 translate-y-full w-full flex items-center justify-center gap-2.5 pb-5 ease-linear duration-200 group-hover:translate-y-0">
           {isPreview ? (
@@ -101,14 +101,14 @@ const ProductItem = ({
               </button>
 
               <button
-                onClick={() => handleAddToCart()}
+                // onClick={() => handleAddToCart()}
                 className="inline-flex font-medium text-custom-sm py-[7px] px-5 rounded-[5px] bg-blue text-white ease-out duration-200 hover:bg-blue-dark"
               >
                 Add to cart
               </button>
 
               <button
-                onClick={() => handleItemToWishList()}
+                // onClick={() => handleItemToWishList()}
                 aria-label="button for favorite select"
                 id="favOne"
                 className="flex items-center justify-center w-9 h-9 rounded-[5px] shadow-1 ease-out duration-200 text-dark bg-white hover:text-blue"
@@ -138,13 +138,12 @@ const ProductItem = ({
         className="w-full flex items-center justify-center font-medium text-dark ease-out duration-200 hover:text-blue mb-1.5"
         onClick={() => handleProductDetails()}
       >
-        <Link href="/shop-details"> {item.title} </Link>
+        <Link href="/shop-details"> {item.name} </Link>
       </h3>
 
       {!isPreview && (
         <span className="flex items-center gap-2 font-medium text-lg">
-          <span className="text-dark">${item.discountedPrice}</span>
-          <span className="text-dark-4 line-through">${item.price}</span>
+          <span className="text-dark">${item.price}</span>
         </span>
       )}
     </div>
